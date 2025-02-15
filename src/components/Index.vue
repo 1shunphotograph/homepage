c43e28bdfb8a3391ce40cd4942c8883c5bcae477
yangguang <274005538@qq.com>
2025-02-15 19:16:28 +0800

    修改背景图片

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -464,7 +464,7 @@ export default {
 .background {
   width: 100%;
   flex: 1;
-  background-image: url("../static/beauty.png");
+  background-image: url("../static/tower.jpg");
   background-size: 100% 100%;
   z-index: 1;
 
